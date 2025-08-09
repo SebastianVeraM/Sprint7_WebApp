fa357e3fc911e2116547217b15e9b71d9a03ce06
Sebastian Vera Morales <sebastianvera4997@gmail.com>
2025-08-09 17:19:00 -0600

    Second version of the web app.

--- app.py
+++ app.py
@@ -1,7 +1,39 @@
 import pandas as pd
 import streamlit as st
-import plotly as pl
+import plotly.express as px
 
 
 # Create app
+df = pd.read_csv(
+    '/Users/guita/Documents/TripleTen/Sprint7_WebApp/vehicles_us.csv')
+
 st.header("Proyecto Sprint 7 - Aplicación Web")
+
+
+car_data = pd.read_csv('vehicles_us.csv')  # leer los datos
+hist_button = st.button('Construir histograma')  # crear un botón
+graph_button = st.button('Construir gráfico de dispersión')  # crear un botón
+
+if hist_button:  # al hacer clic en el botón
+    # escribir un mensaje
+    st.write(
+        'Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+
+    # crear un histograma
+    fig = px.histogram(car_data, x="odometer")
+
+    # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig, use_container_width=True)
+
+    hist_button = st.button('Construir histograma')  # crear un botón
+
+if graph_button:  # al hacer clic en el botón
+    # escribir un mensaje
+    st.write(
+        'Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+
+    # crear un histograma
+    fig = px.histogram(car_data, x="odometer")
+
+    # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig, use_container_width=True)
